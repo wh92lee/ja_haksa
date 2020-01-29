204ce4ea1265dcc2792d54d4df957433a5fac304
fkenl123 <fkenl123@gmail.com>
2020-01-29 12:44:57 +0900

    by lee

---
+++ joongang/src/main/java/oracle/java/joongang/dao/CahtBotDaoImpl.java
@@ -0,0 +1,5 @@
+package oracle.java.joongang.dao;
+
+public class CahtBotDaoImpl implements ChatBotDao {
+
+}
---
+++ joongang/src/main/java/oracle/java/joongang/dao/ChatBotDao.java
@@ -0,0 +1,5 @@
+package oracle.java.joongang.dao;
+
+public interface ChatBotDao {
+	
+}
