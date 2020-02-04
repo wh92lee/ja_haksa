77f6d81f3071a84288dd5952f6be69a519a18afd
이원희 <@DESKTOP-K9BN10B>
2020-02-04 13:34:07 +0900

    Lee

--- joongang/src/main/java/oracle/java/joongang/model/City.java
+++ joongang/src/main/java/oracle/java/joongang/model/City.java
@@ -19,7 +19,7 @@ public class City {
 	public String getCity_name() {
 		return city_name;
 	}
-	public void setCity_name(String city_name) {
+	public void setCity_name(String city_name) { 
 		this.city_name = city_name;
 	} 
 }
