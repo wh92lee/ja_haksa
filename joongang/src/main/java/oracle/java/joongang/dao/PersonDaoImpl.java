8d229d1eb1f20235e344156ea7f78e0661c8565b
fkenl123 <fkenl123@gmail.com>
2020-02-21 17:46:35 +0900

    by lee 02.21 17:46

--- joongang/src/main/java/oracle/java/joongang/dao/PersonDaoImpl.java
+++ joongang/src/main/java/oracle/java/joongang/dao/PersonDaoImpl.java
@@ -1,9 +1,12 @@
 package oracle.java.joongang.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import oracle.java.joongang.model.City;
 import oracle.java.joongang.model.Person;
 
 @Repository
@@ -35,4 +38,29 @@ public class PersonDaoImpl implements PersonDao {
 		return session.selectOne("aliveChk", person);
 	}
 
+	@Override
+	public List<City> b_city() {
+		// TODO Auto-generated method stub
+		return session.selectList("b_city");
+	}
+
+	@Override
+	public List<City> getmcity(int b_city) {
+		// TODO Auto-generated method stub
+		return session.selectList("m_city", b_city);
+	}
+
+	@Override
+	public int idcheck(String pid) {
+		// TODO Auto-generated method stub
+		return session.selectOne("idcheck", pid);
+	}
+
+	@Override
+	public int addstudent(Person person) {
+		// TODO Auto-generated method stub
+		return session.update("addstudent", person);
+	}
+
 }
+
